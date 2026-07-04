ca17ccb905f013ad22946af852b13a1959a455f9
Keerthi0510-naga <keerthinaga0510@gmail.com>
2026-07-04 16:30:47 +0530

    Completed HackerRank Day 25 running time and coplexity

---
+++ HackerRank/day_25_Running_Time _and_Complexity.py
@@ -0,0 +1,20 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+
+    if n < 2:
+        print("Not prime")
+    else:
+        is_prime = True
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print("Prime")
+        else:
+            print("Not prime")
